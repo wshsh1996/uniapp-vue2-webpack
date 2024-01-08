5af4617b1a8a11d93262ebd35d1ecad29e244b61
‘王帅’ <2122008362@qq.com>
2024-01-08 12:00:55 +0800

    feat: uniapp + vue2

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-essential', '@vue/eslint-config-prettier'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  // 小程序全局变量
+  globals: {
+    uni: true,
+    wx: true,
+    WechatMiniprogram: true,
+    getCurrentPages: true,
+    getApp: true,
+    UniApp: true,
+    UniHelper: true,
+    App: true,
+    Page: true,
+    Component: true,
+    AnyObject: true
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['vue'],
+  rules: {
+    'vue/multi-word-component-names': ['off'],
+    'vue/no-setup-props-destructure': ['off'],
+    'vue/no-deprecated-html-element-is': ['off'],
+    '@typescript-eslint/no-unused-vars': ['off'],
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        tabWidth: 2,
+        useTabs: false,
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+        proseWrap: 'preserve'
+      }
+    ]
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App'
-import './uni.promisify.adaptor'
+import './uni.promisify.adaptor' // 适配器，将原生api转为promise形式，方便异步编程
+import store from '@/store'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false // 阻止启动生产消息
 
-App.mpType = 'app'
+App.mpType = 'app' // 当前运行的是原生小程序
 
 const app = new Vue({
+  store,
   ...App
 })
 app.$mount()
